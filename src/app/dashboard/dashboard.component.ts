31593ce208cce830601bd0da462629948bc8bd9f
jotad9 <jotadaniel2003@gmail.com>
2024-10-30 11:22:56 +0100

    create footer and cards to home

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HttpClientModule],
+  imports: [RouterOutlet, SidebarComponent, HttpClientModule, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
